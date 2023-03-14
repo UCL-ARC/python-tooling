53e851999074567f89a48dce56c8a691d1619f8f
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-03-14 14:24:27 +0000

    Add dummy test and fix ruff

--- tests/test_package_gen.py
+++ tests/test_package_gen.py
@@ -1,9 +1,10 @@
 import subprocess
 
-def test_package_generation(tmpdir):
-    subprocess.run(['cookieninja', '.', '--no-input', '--output-dir', str(tmpdir)])
 
-    assert (tmpdir / 'python-template').exists()
-    expected_files = ['README.md']
+def test_package_generation(tmpdir):
+    subprocess.run(["cookieninja", ".", "--no-input", "--output-dir", str(tmpdir)])
+
+    assert (tmpdir / "python-template").exists()
+    expected_files = ["README.md"]
     for f in expected_files:
-        assert (tmpdir / 'python-template' / f).exists()
+        assert (tmpdir / "python-template" / f).exists()
--- {{cookiecutter.project_slug}}/tests/conftest.py
+++ {{cookiecutter.project_slug}}/tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 @pytest.fixture(scope="session")
 def dummy_fixture():
     """
