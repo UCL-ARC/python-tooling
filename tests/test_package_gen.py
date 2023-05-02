80f656083795a923ca3836e3ea3c92de0dbd36ef
sfmig <33267254+sfmig@users.noreply.github.com>
2023-05-02 12:57:05 +0100

    draft test that runs cookieninja for sample project name and checks expected files are generated (failing)
    Co-authored-by: yidilozdemir <yidilozdemir@users.noreply.github.com>

--- tests/test_package_gen.py
+++ tests/test_package_gen.py
@@ -1,13 +1,38 @@
+import pathlib as pl
 import subprocess
+
+PROJECT_SLUG = "cookiecutter_test"
 
 
 def test_package_generation(tmpdir):
+    # Run cookieninja with PROJECT_SLUG for project_slug
     subprocess.run(
-        ["cookieninja", ".", "--no-input", "--output-dir", str(tmpdir)],  # noqa: S607
+        [  # noqa: S607
+            "cookieninja",
+            ".",
+            "--no-input",
+            "--output-dir",
+            str(tmpdir),
+            "project_slug=",
+            PROJECT_SLUG,
+        ],
         shell=False,  # noqa: S603
     )
 
+    # Check parent directory of the repo exists
     assert (tmpdir / "python-template").exists()
-    expected_files = ["README.md"]
+
+    # Check files and directories inside
+    expected_files = [
+        "README.md",
+        ".pre-commit-config.yaml",
+        # "LICENSE",
+        "pyproject.toml",
+        # Directories
+        pl.Path("src") / PROJECT_SLUG,
+        "tests",
+        pl.Path(".github"),
+        pl.Path(".github") / "workflows",
+    ]
     for f in expected_files:
-        assert (tmpdir / "python-template" / f).exists()
+        assert (tmpdir / "python-template" / PROJECT_SLUG / f).exists()
