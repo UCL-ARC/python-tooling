b22a5073c9fe5425b1abe46651df620df865cb37
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-05-02 11:34:52 +0100

    Add various things I've learnt from last time

--- tests/test_package_gen.py
+++ tests/test_package_gen.py
@@ -1,7 +1,19 @@
+"""Checks that the cookiecutter works."""
+
 import subprocess
+from pathlib import Path
 
 
-def test_package_generation(tmpdir):
+def test_package_generation(tmpdir: Path):
+    """
+    Creates a project cookiecutter from the template.
+
+    Once the project is made it verifies a series of actions work.
+
+    Args:
+    ----
+        tmpdir: A temporary directory path object which is unique.
+    """
     subprocess.run(["cookieninja", ".", "--no-input", "--output-dir", str(tmpdir)])
 
     assert (tmpdir / "python-template").exists()
