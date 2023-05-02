fa25044e39b22a766a367e81e8b4b6a321caa6c2
sfmig <33267254+sfmig@users.noreply.github.com>
2023-05-02 16:14:52 +0100

    tests passing now

--- tests/test_package_gen.py
+++ tests/test_package_gen.py
@@ -4,7 +4,10 @@ import subprocess
 PROJECT_SLUG = "cookiecutter_test"
 
 
-def test_package_generation(tmpdir):
+def test_package_generation(tmp_path):
+    # tmp_path fixture preferred over tmpdir
+    # see https://docs.pytest.org/en/7.3.x/how-to/tmp_path.html#the-tmpdir-and-tmpdir-factory-fixtures
+
     # Run cookieninja with PROJECT_SLUG for project_slug
     subprocess.run(
         [  # noqa: S607
@@ -12,15 +15,14 @@ def test_package_generation(tmpdir):
             ".",
             "--no-input",
             "--output-dir",
-            str(tmpdir),
-            "project_slug=",
-            PROJECT_SLUG,
+            str(tmp_path),
+            f"project_slug={PROJECT_SLUG}",
         ],
         shell=False,  # noqa: S603
     )
 
-    # Check parent directory of the repo exists
-    assert (tmpdir / "python-template").exists()
+    # Check parent directory exists
+    assert (tmp_path / PROJECT_SLUG).exists()
 
     # Check files and directories inside
     expected_files = [
@@ -29,10 +31,11 @@ def test_package_generation(tmpdir):
         # "LICENSE",
         "pyproject.toml",
         # Directories
+        "src",
         pl.Path("src") / PROJECT_SLUG,
         "tests",
         pl.Path(".github"),
         pl.Path(".github") / "workflows",
     ]
     for f in expected_files:
-        assert (tmpdir / "python-template" / PROJECT_SLUG / f).exists()
+        assert (tmp_path / PROJECT_SLUG / f).exists()
