e403b53ad6d09c9d0902a095a72d79a53318890e
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-10-24 12:31:57 +0100

    COmment out shit

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -19,15 +19,15 @@ def main(initialise_git_repository: str) -> int:
     if initialise_git_repository == "True":
         cmd = ["git", "init"]
         result = subprocess.run(cmd, check=True)  # noqa: S603
-        print(
-            "You now have a local git repository. To sync this to GitHub you need "
-            "to create an empty GitHub repo with the name "
-            "{{cookiecutter.project_slug}} - DO NOT SELECT ANY OTHER OPTION. "
-            "Alternatively, if you have the GitHub CLI installed "
-            "https://cli.github.com, then you can run "
-            "gh repo create {{cookiecutter.project_slug}} -d "
-            "{{cookiercutter.description}} --public -r origin --source .",
-        )
+        # print(
+        #     "You now have a local git repository. To sync this to GitHub you need "
+        #     "to create an empty GitHub repo with the name "
+        #     "{{cookiecutter.project_slug}} - DO NOT SELECT ANY OTHER OPTION. "
+        #     "Alternatively, if you have the GitHub CLI installed "
+        #     "https://cli.github.com, then you can run "
+        #     "gh repo create {{cookiecutter.project_slug}} -d "
+        #     "{{cookiercutter.description}} --public -r origin --source .",
+        # )
         return result.returncode
     return 0
 
