0f9d579037545ce2cd04429a9fb5515a1888a833
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-10-24 12:00:42 +0100

    Add return code

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,12 +1,19 @@
 """Post project generation hook."""
 
 import subprocess
+import sys
 
 
-def main() -> None:
-    """Create a git repository on generation of the project."""
+def main() -> int:
+    """
+    Create a git repository on generation of the project.
+
+    Returns
+    -------
+        The return code of the process
+    """
     cmd = ["git", "init"]
-    subprocess.run(cmd, check=True)  # noqa: S603
+    result = subprocess.run(cmd, check=True)  # noqa: S603
     print(
         "You now have a local git repository. To sync this to GitHub you need "
         "to create an empty GitHub repo with the name "
@@ -16,7 +23,9 @@ def main() -> None:
         "gh repo create {{cookiecutter.project_slug}} -d "
         "{{cookiercutter.description}} --public -r origin --source .",
     )
+    return result.returncode
 
 
 if __name__ == "__main__":
-    main()
+    if "{{cookiecutter.initialise_git_repository}}":
+        sys.exit(main())
