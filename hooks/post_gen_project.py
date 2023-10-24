94a33a5ead7c004ab68562b4837c05277727042f
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-10-24 12:25:58 +0100

    Change to string

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,28 +4,33 @@ import subprocess
 import sys
 
 
-def main() -> int:
+def main(initialise_git_repository: str) -> int:
     """
     Create a git repository on generation of the project.
 
-    Returns
+    Args:
+    ----
+        initialise_git_repository: Whether to initialise the repo
+
+    Returns:
     -------
         The return code of the process
     """
-    cmd = ["git", "init"]
-    result = subprocess.run(cmd, check=True)  # noqa: S603
-    print(
-        "You now have a local git repository. To sync this to GitHub you need "
-        "to create an empty GitHub repo with the name "
-        "{{cookiecutter.project_slug}} - DO NOT SELECT ANY OTHER OPTION. "
-        "Alternatively, if you have the GitHub CLI installed "
-        "https://cli.github.com, then you can run "
-        "gh repo create {{cookiecutter.project_slug}} -d "
-        "{{cookiercutter.description}} --public -r origin --source .",
-    )
-    return result.returncode
+    if initialise_git_repository == "True":
+        cmd = ["git", "init"]
+        result = subprocess.run(cmd, check=True)  # noqa: S603
+        print(
+            "You now have a local git repository. To sync this to GitHub you need "
+            "to create an empty GitHub repo with the name "
+            "{{cookiecutter.project_slug}} - DO NOT SELECT ANY OTHER OPTION. "
+            "Alternatively, if you have the GitHub CLI installed "
+            "https://cli.github.com, then you can run "
+            "gh repo create {{cookiecutter.project_slug}} -d "
+            "{{cookiercutter.description}} --public -r origin --source .",
+        )
+        return result.returncode
+    return 0
 
 
 if __name__ == "__main__":
-    if "{% cookiecutter.initialise_git_repository %}":
-        sys.exit(main())
+    sys.exit(main("{{ cookiecutter.initialise_git_repository }}"))
