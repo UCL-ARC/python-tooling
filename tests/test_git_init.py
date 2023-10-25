2a3b6a85c33fd8084473b4eb0192c56d983968c9
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-10-25 16:39:36 +0100

    Ignore rules for `ruff-format`

---
+++ tests/test_git_init.py
@@ -0,0 +1,37 @@
+"""Checks that the git repo initialisation works."""
+
+import pathlib
+import subprocess
+
+
+def test_git_not_initialised(
+    tmp_path: pathlib.Path,
+    project_config: dict,
+):
+    """ """
+    # Run cookiecutter with project_slug set to the value in the project config
+    subprocess.run(
+        [  # noqa: S607
+            "cookiecutter",
+            ".",
+            "--no-input",
+            "--output-dir",
+            str(tmp_path),
+            "initialise_git_repository=false",
+            f"project_name={project_config['project_name']}",
+        ],
+        check=False,
+    )
+
+    test_project_dir = tmp_path / project_config["expected_repo_name"]
+
+    result = subprocess.run(
+        ["git", "-C", str(test_project_dir), "status"],
+        capture_output=True,
+        check=False,
+    )
+    assert (
+        result.returncode != 0
+        and result.stderr
+        == b"fatal: not a git repository (or any of the parent directories): .git\n"
+    ), "git initialised when it shouldn't be"
