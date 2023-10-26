13cbd2fcc0c2520755a352f2cbed83c7c022aac6
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-10-26 10:39:22 +0100

    Parameterise test

--- tests/test_git_init.py
+++ tests/test_git_init.py
@@ -3,18 +3,25 @@
 import pathlib
 import subprocess
 
+import pytest
 
-def test_git_not_initialised(
-    tmp_path: pathlib.Path,
-    project_config: dict,
+
+@pytest.mark.parametrize(
+    "git_init_cookiecutter_option",
+    ["initialise_git_repository=True", "initialise_git_repository=False"],
+)
+def test_initialisation_of_git_repo(
+    tmp_path: pathlib.Path, project_config: dict, git_init_cookiecutter_option: str
 ) -> None:
     """
-    Checks for the case without git initialisation.
+    Checks to see if git was correctly initialised if desired.
 
     Args:
         tmp_path: A temporary directory path object which is unique.
         project_config: A dictionary with values for the cookiecutter template,
             as defined in the cookiecutter.json
+        git_init_cookiecutter_option: A string defined in cookiecutter.json,
+            which determines if git should be initialised or not
     """
     # Run cookiecutter with `initialise_git_repository=False`
     subprocess.run(
@@ -25,7 +32,7 @@ def test_git_not_initialised(
             "--output-dir",
             f"{tmp_path}",
             f"project_name={project_config['project_name']}",
-            "initialise_git_repository=False",
+            git_init_cookiecutter_option,
         ],
         check=True,
     )
@@ -43,50 +50,12 @@ def test_git_not_initialised(
         check=False,
         text=True,
     )
-    assert (
-        result.stderr
-        == "fatal: not a git repository (or any of the parent directories): .git\n"
-    ), f"Error: {result.stderr!r}"
 
-
-def test_git_initialised(
-    tmp_path: pathlib.Path,
-    project_config: dict,
-) -> None:
-    """
-    Checks for the case with git initialisation.
-
-    Args:
-        tmp_path: A temporary directory path object which is unique.
-        project_config: A dictionary with values for the cookiecutter template,
-            as defined in the cookiecutter.json
-    """
-    # Run cookiecutter with `initialise_git_repository=True`
-    subprocess.run(
-        [  # noqa: S603,S607
-            "cookiecutter",
-            ".",
-            "--no-input",
-            "--output-dir",
-            f"{tmp_path}",
-            f"project_name={project_config['project_name']}",
-            "initialise_git_repository=True",
-        ],
-        check=True,
-        capture_output=True,
-    )
-
-    test_project_dir = tmp_path / project_config["expected_repo_name"]
-
-    result = subprocess.run(
-        [  # noqa: S603,S607
-            "git",
-            "-C",
-            f"{test_project_dir}",
-            "status",
-        ],
-        capture_output=True,
-        check=False,
-        text=True,
-    )
-    assert result.returncode == 0, f"Error: {result.stderr!r}"
+    if "True" in git_init_cookiecutter_option:
+        assert result.returncode == 0
+    else:
+        # should not have found repo
+        assert (
+            result.stderr
+            == "fatal: not a git repository (or any of the parent directories): .git\n"
+        )
