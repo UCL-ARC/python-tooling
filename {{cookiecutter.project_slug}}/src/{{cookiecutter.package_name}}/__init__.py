cf38e854146a22e368e32bfdf30b41a8dd208ae8
Matt Graham <matthew.m.graham@gmail.com>
2024-03-20 16:20:54 +0000

    Add example function with docstring to template package

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -1,2 +1,12 @@
 """{{cookiecutter.package_name}} package."""
 from ._version import __version__  # noqa: F401
+
+
+def example_function(argument: str, keyword_argument: str = "default") -> None:
+    """An example function docstring.
+
+    :param argument: An argument.
+    :param keyword_argument: A keyword argument with a default value.
+    :returns: The concatenation of `argument` and `keyword_argument`.
+    """
+    return argument + keyword_argument
