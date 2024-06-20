5a498b7b9e87bc9b6592fc1a638b86736a5f038b
David Stansby <dstansby@gmail.com>
2024-06-20 08:21:52 +0100

    Refactor tests (#405)
    This makes the tests a bit easier to extend by:
    
    - Defining a helper ~~function~~ **_fixture_** to generate a package
    from a given set of configuration options (in ~~`tests/helpers.py`~~
    `conftest.py`)
    - Defining the configuration within the tests that are being used,
    instead of in `conftest.py`. This makes it easier to see what config
    each test is using next to the actual test.
    
    ---------
    
    Co-authored-by: Sam Cunliffe <s.cunliffe@ucl.ac.uk>
    Co-authored-by: Sam Cunliffe <samcunliffe@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,39 @@
-"""Defines fixtures for tests."""
+"""Fixtures for the cookiecutter template tests."""
+
+import pathlib
+import subprocess
+import typing
 
 import pytest
 
 
-@pytest.fixture()
-def project_config() -> dict:
+def _generate_package(
+    config: dict[str, str], path: pathlib.Path
+) -> subprocess.CompletedProcess[str]:
     """
-    Pytest fixture for defining the project config.
+    Generate a project from the cookiecutter template.
 
-    Returns
-    -------
-    dict
-        dictionary with values for the cookiecutter template,
+    Arguments:
+    ---------
+    config: dict
+        A dictionary with values for the cookiecutter template,
         as defined in the cookiecutter.json
+    path: Path
+        Directory to create package in.
 
     """
-    return {
-        "github_username": "test-user",
-        "project_short_description": "description",
-        "project_name": "Cookiecutter Test",
-        "expected_repo_name": "cookiecutter-test",
-        "expected_package_name": "cookiecutter_test",
-    }
+    args = [f"{key}={val}" for key, val in config.items()]
+    cmd = ["cookiecutter", ".", "--no-input", "--output-dir", f"{path}"]
+    return subprocess.run(
+        cmd + args,
+        check=False,
+        shell=False,  # noqa: S603
+        capture_output=True,
+        text=True,
+    )
+
+
+@pytest.fixture()
+def generate_package() -> typing.Callable:
+    """Generate project from cookiecutter template."""
+    return _generate_package
