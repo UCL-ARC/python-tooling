79ca8505b392a84a318dd76b3a3e4f26469e79b9
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-10-11 15:29:09 +0100

    Revert "`__pycache__` can feature anywhere"
    This reverts commit 7d99c7c36a4a4bf35488121516049b5494bb1ce8.

--- tests/test_package_generation.py
+++ tests/test_package_generation.py
@@ -79,10 +79,11 @@ def test_package_generation(
 
     actual_files = get_all_files_folders(test_project_dir)
     # Filter out anything under specific directories to make comparison easier
-    actual_files = {
-        path
-        for path in actual_files
-        if not (str(path).startswith(".git/") or "__pycache__" in path.parts)
+    actual_files = actual_files - {
+        a
+        for a in actual_files
+        if len(a.parts) > 0
+        and (a.parts[0] == ".git" or "__pycache__" in a.parts or ".DS_Store" in a.parts)
     }
 
     assert actual_files == expected_files
