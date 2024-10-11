5ea14b2bd9442d2f87a4efd89f62876a77509f45
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-10-11 13:06:20 +0100

    Check length

--- tests/test_package_generation.py
+++ tests/test_package_generation.py
@@ -81,7 +81,9 @@ def test_package_generation(
     actual_files = get_all_files_folders(test_project_dir)
     # Filter out anything under specific directories to make comparison easier
     actual_files = actual_files - {
-        a for a in actual_files if a.parts[0] in (".git", "__pycache__")
+        a
+        for a in actual_files
+        if len(a.parts) > 0 and a.parts[0] in (".git", "__pycache__")
     }
 
     assert actual_files == expected_files
