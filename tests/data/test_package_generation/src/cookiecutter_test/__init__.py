c956a4eacda2832c92fcce5f4d79178d19f21e57
Patrick Roddy <patrickjamesroddy@gmail.com>
2026-06-12 12:30:30 +0100

    Swap out `mypy` for `ty` in code

--- tests/data/test_package_generation/src/cookiecutter_test/__init__.py
+++ tests/data/test_package_generation/src/cookiecutter_test/__init__.py
@@ -1,6 +1,6 @@
 """cookiecutter_test package."""
 
-from ._version import __version__  # noqa: F401
+from ._version import __version__  # noqa: F401  # ty: ignore[unresolved-import]
 
 
 def example_function(argument: str, keyword_argument: str = "default") -> str:
